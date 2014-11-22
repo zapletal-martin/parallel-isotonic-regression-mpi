3d7db9b96aa114c50958a881b2bf751c9a153a2f
martinzapletal <zapletal-martin@email.cz>
2014-11-22 15:49:45 +0000

    Optimization of isotonic regression algorithm to pool continuous violating arrays at once and to return back to pool previous violators

--- hellow2.c
+++ hellow2.c
@@ -8,44 +8,6 @@ struct LabeledPoint
    double feature;
 } labeledPoint; 
 
-
-int sum(struct LabeledPoint *input, int length) {
-   int sum = 0;
-   int i = 0;
-
-   for(i = 0; i < length; i++) {
-      sum += input[i].label;
-   }  
-
-   return sum;
-}
-
-void pool(struct LabeledPoint *input, int length) {
-   int i;
-
-   double sumCache = sum(input, length);
-
-   for(i = 0; i < length; i++) {
-      input[i].label = sumCache / length;
-   }
-}
-
-struct LabeledPoint *pava(struct LabeledPoint *input, int length) {
-	int i;
-
-   for(i = 0; i < length; i++) {
-      int j = 0;
-
-      for(j = i; j > 0; j--) {
-         if(input[j].label < input[j-1].label) {
-            pool(&input[j-1], i-j+2);
-         }
-      }
-   }
-
-   return input;
-}
-
 void printArray(struct LabeledPoint *input, int length) {
    printf("\r\n-------------------------\r\n");
    
@@ -54,6 +16,54 @@ void printArray(struct LabeledPoint *input, int length) {
      printf("%f ", input[j].label);
    }
    printf("\r\n-------------------------\r\n");
+}
+
+int sum(struct LabeledPoint *input, int start, int end) {
+   int sum = 0;
+
+   for(; start <= end; start++) {
+      sum += input[start].label;
+   }  
+
+   return sum;
+}
+
+void pool(struct LabeledPoint *input, int start, int end) {
+   double weightedSum = sum(input, start, end);
+   double weight = end - start + 1;
+
+   for(; start <= end; start++) {
+      input[start].label = weightedSum / weight;
+   }
+}
+
+struct LabeledPoint *pava(struct LabeledPoint *input, int length) {
+   int i = 0;
+
+   while(i < length) {
+      int j = i;
+
+      //find monotonicity violating sequence, if any
+      while(j < length - 1 && !(input[j].label <= input[j + 1].label)) {
+        j = j + 1;
+      }
+
+      //if monotonicity was not violated, move to next data point
+      if(i == j) {
+        i = i + 1;
+      } else {
+        //otherwise pool the violating sequence
+        //and check if pooling caused monotonicity violation in previously processed points
+        while (i >= 0 && !(input[i].label <= input[i + 1].label)) {
+          pool(input, i, j);
+          i = i - 1;
+        }
+
+        i = j;
+      }
+   }
+
+   return input;
 }
 
 int main(argc, argv)
@@ -67,6 +77,25 @@ char *argv[];
    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
    MPI_Comm_size(MPI_COMM_WORLD, &size);
    MPI_Get_processor_name(name, &length);
+
+   if (size < 2) {
+      //TODO SIMPLE PAVA
+      fprintf(stderr,"Requires at least two processes.\n");
+      exit(-1);
+   }
+
+   //Initialize MPI struct for LabeledPoint
+   //const int nitems = 2;
+   //int          blocklengths[2] = {1, 1};
+   //MPI_Datatype types[2] = {MPI_DOUBLE, MPI_DOUBLE};
+   //MPI_Datatype mpi_LabeledPoint_type;
+   //MPI_Aint     offsets[2];
+
+   //offsets[0] = offsetof(labeledPoint, label);
+   //offsets[1] = offsetof(labeledPoint, feature);
+
+   //MPI_Type_create_struct(nitems, blocklengths, offsets, types, &mpi_car_type);
+   //MPI_Type_commit(&mpi_car_type);
 
    printf("name %s: hello world from process %d of %d\n", name, rank, size);
 
