fc876bbc64d2e6200b26f84690e0c790a4f774d3
martinzapletal <zapletal-martin@email.cz>
2014-11-29 21:07:28 -0500

    Added weight to the LabeledPoint structure and fixed offset bug

--- hellow2.c
+++ hellow2.c
@@ -3,10 +3,13 @@
 #include <limits.h>
 #include <mpi.h>    /* all MPI-2 functions defined there */
 
+#define MAX_PARTITION_SIZE 1048576
+
 typedef struct LabeledPoint
 {
    double label;
    double feature;
+   double weight;
 } LabeledPointT; 
 
 void printArray(LabeledPointT *input, int length) {
@@ -63,7 +66,7 @@ LabeledPointT *pava(LabeledPointT *input, int length) {
         i = j;
       }
    }
-
+  
    return input;
 }
 
@@ -84,28 +87,32 @@ MPI_Datatype MPI_Init_Type_LabeledPoint () {
   struct LabeledPoint _labeledPoint;
 
   int count; //Says how many kinds of data your structure has
-  count = 2; //1, 'cause you just have int
+  count = 3; 
 
   //Says the type of every block
   MPI_Datatype array_of_types[count];
   //You just have int
   array_of_types[0] = MPI_DOUBLE;
   array_of_types[1] = MPI_DOUBLE;
+  array_of_types[2] = MPI_DOUBLE;
 
   //Says how many elements for block
   int array_of_blocklengths[count];
   //You have 8 int
   array_of_blocklengths[0] = 1;
   array_of_blocklengths[1] = 1;
+  array_of_blocklengths[2] = 1;
 
   /*Says where every block starts in memory, counting from the beginning of the struct.*/
   MPI_Aint array_of_displaysments[count];
-  MPI_Aint address1, address2, address3;
+  MPI_Aint address1, address2, address3, address4;
   MPI_Get_address(&_labeledPoint, &address1);
   MPI_Get_address(&_labeledPoint.label, &address2);
   MPI_Get_address(&_labeledPoint.feature, &address3);
+  MPI_Get_address(&_labeledPoint.weight, &address4);
   array_of_displaysments[0] = address2 - address1;
-  array_of_displaysments[1] = address3 - address2;
+  array_of_displaysments[1] = address3 - address1;
+  array_of_displaysments[2] = address4 - address1;
   //array_of_displaysments[0] = offsetof(_labeledPoint, label);
   //array_of_displaysments[1] = offsetof(_labeledPoint, feature);
 
@@ -142,49 +149,46 @@ char *argv[];
    printf("name %s: hello world from process %d of %d\n", name, rank, size);
 
    if(rank == 0) {
-      LabeledPointT labels[21] = {{1, 1} , {2, 2}, {3, 3}, {3, 4}, {1, 5}, {6, 6}, {7, 7}, {8, 8}, {11, 9}, {9, 10}, {10, 11}, {12, 12}, {14, 13}, {15, 14}, {17, 15}, {16, 16}, {17, 17}, {18, 18}, {19, 19}, {20, 20}, {21, 21}};
+      LabeledPointT labels[21] = {{1, 1, 1} , {2, 2, 1}, {3, 3, 1}, {3, 4, 1}, {1, 5, 1}, {6, 6, 1}, {7, 7, 1}, {8, 8, 1}, {11, 9, 1}, {9, 10, 1}, {10, 11, 1}, {12, 12, 1}, {14, 13, 1}, {15, 14, 1}, {17, 15, 1}, {16, 16, 1}, {17, 17, 1}, {18, 18, 1}, {19, 19, 1}, {20, 20, 1}, {21, 21, 1}};
 
       int destination;
       int partition = 0;
-
+      
       //distribute to workers
       for(destination = 1; destination < size; destination++) {
         MPI_Send(labels + partition, 7, MPI_LabeledPoint, destination, 1, MPI_COMM_WORLD);
         partition += 7;
       }
-
+      
       MPI_Status status;
-      LabeledPointT result[20];
+      LabeledPointT result[21];
+      int count;
       partition = 0;
       for(destination = 1; destination < size; destination++) {
-         MPI_Recv(result + partition, INT_MAX, MPI_LabeledPoint, destination, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-         partition += 7;
+         MPI_Recv(result + partition, MAX_PARTITION_SIZE, MPI_LabeledPoint, destination, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+         MPI_Get_count(&status, MPI_LabeledPoint, &count);
+         //printf("RECEIVED COUNT %d\n", count);
+         partition += count;
       }
 
-      printArray(result, 20);
-      LabeledPointT *finalResult = pava(result, 20);
+      LabeledPointT *finalResult = pava(result, 21);
 
-      printArray(finalResult, 20);
+      printArray(finalResult, 21);
 
    } else {
       MPI_Status status;
       int count;
-      LabeledPointT work[20];
+      //LabeledPointT buffer[MAX_PARTITION_SIZE];
 
-      //MPI_Get_count(&status, MPI_LabeledPoint, &count);
-      //work = malloc(count * sizeof(LabeledPointT));
+      //TODO: LEAKING
+      LabeledPointT *buffer = malloc(MAX_PARTITION_SIZE * sizeof(LabeledPointT));
 
-      MPI_Recv(&work, INT_MAX, MPI_LabeledPoint, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-      
-      //printArray(work, 5);
+      MPI_Recv(buffer, MAX_PARTITION_SIZE, MPI_LabeledPoint, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+      MPI_Get_count(&status, MPI_LabeledPoint, &count);
 
-      LabeledPointT *result = pava(work, 7);
+      LabeledPointT *result = pava(buffer, count);
 
-      MPI_Send(result, 7, MPI_LabeledPoint, 0, 1, MPI_COMM_WORLD);
-      //LabeledPointT *newlabels;
-     
-      //newlabels = pava(labels, 20);
-     // printArray(newlabels, 20);
+      MPI_Send(result, count, MPI_LabeledPoint, 0, 1, MPI_COMM_WORLD);
    }
 
    MPI_Type_free(&MPI_LabeledPoint);
