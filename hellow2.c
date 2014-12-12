70090c564c82aba89132f53a36eac4b43dc4b175
martinzapletal <zapletal-martin@email.cz>
2014-12-11 22:50:25 -0500

    Refactor to functions for modularity

--- hellow2.c
+++ hellow2.c
@@ -51,7 +51,7 @@ void pool(LabeledPointT *input, int start, int end) {
    }
 }
 
-LabeledPointT *pava(LabeledPointT *input, int length) {
+LabeledPointT *poolAdjacentViolators(LabeledPointT *input, int length) {
    int i = 0;
 
    while(i < length) {
@@ -81,39 +81,22 @@ LabeledPointT *pava(LabeledPointT *input, int length) {
 }
 
 MPI_Datatype MPI_Init_Type_LabeledPoint () {
-   //Initialize MPI struct for LabeledPoint
-   //const int nitems = 2;
-   //int          blocklengths[2] = {1, 1};
-   //MPI_Datatype types[2] = {MPI_DOUBLE, MPI_DOUBLE};
-   //MPI_Datatype mpi_LabeledPoint_type;
-   //MPI_Aint     offsets[2];
-
-   //offsets[0] = offsetof(labeledPoint, label);
-   //offsets[1] = offsetof(labeledPoint, feature);
-
-   //MPI_Type_create_struct(nitems, blocklengths, offsets, types, &mpi_car_type);
-   //MPI_Type_commit(&mpi_car_type);
-
   struct LabeledPoint _labeledPoint;
 
-  int count; //Says how many kinds of data your structure has
+  int count; 
   count = 3; 
 
-  //Says the type of every block
   MPI_Datatype array_of_types[count];
-  //You just have int
+
   array_of_types[0] = MPI_DOUBLE;
   array_of_types[1] = MPI_DOUBLE;
   array_of_types[2] = MPI_DOUBLE;
 
-  //Says how many elements for block
   int array_of_blocklengths[count];
-  //You have 8 int
   array_of_blocklengths[0] = 1;
   array_of_blocklengths[1] = 1;
   array_of_blocklengths[2] = 1;
 
-  /*Says where every block starts in memory, counting from the beginning of the struct.*/
   MPI_Aint array_of_displaysments[count];
   MPI_Aint address1, address2, address3, address4;
   MPI_Get_address(&_labeledPoint, &address1);
@@ -123,17 +106,55 @@ MPI_Datatype MPI_Init_Type_LabeledPoint () {
   array_of_displaysments[0] = address2 - address1;
   array_of_displaysments[1] = address3 - address1;
   array_of_displaysments[2] = address4 - address1;
-  //array_of_displaysments[0] = offsetof(_labeledPoint, label);
-  //array_of_displaysments[1] = offsetof(_labeledPoint, feature);
 
-  /*Create MPI Datatype and commit*/
   MPI_Datatype mpi_LabeledPoint_type;
   MPI_Type_create_struct(count, array_of_blocklengths, array_of_displaysments, array_of_types, &mpi_LabeledPoint_type);
   MPI_Type_commit(&mpi_LabeledPoint_type);
 
   return mpi_LabeledPoint_type;
-  //Now we are ready to send
-  //MPI_Send(&_info, 1, stat_type, dest, tag, comm),
+}
+
+void master(MPI_Datatype MPI_LabeledPoint, int size) {
+  LabeledPointT labels[21] = {{1, 1, 1} , {2, 2, 1}, {3, 3, 1}, {3, 4, 1}, {1, 5, 1}, {6, 6, 1}, {7, 7, 1}, {8, 8, 1}, {11, 9, 1}, {9, 10, 1}, {10, 11, 1}, {12, 12, 1}, {14, 13, 1}, {15, 14, 1}, {17, 15, 1}, {16, 16, 1}, {17, 17, 1}, {18, 18, 1}, {19, 19, 1}, {20, 20, 1}, {21, 21, 1}};
+
+  int destination;
+  int partition = 0;
+  
+  //distribute to workers
+  for(destination = 1; destination < size; destination++) {
+    MPI_Send(labels + partition, 7, MPI_LabeledPoint, destination, 1, MPI_COMM_WORLD);
+    partition += 7;
+  }
+  
+  MPI_Status status;
+  LabeledPointT result[21];
+  int count;
+  partition = 0;
+  for(destination = 1; destination < size; destination++) {
+     MPI_Recv(result + partition, MAX_PARTITION_SIZE, MPI_LabeledPoint, destination, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+     MPI_Get_count(&status, MPI_LabeledPoint, &count);
+     //printf("RECEIVED COUNT %d\n", count);
+     partition += count;
+  }
+
+  LabeledPointT *finalResult = poolAdjacentViolators(result, 21);
+
+  printArray(finalResult, 21);
+}
+
+void partition(MPI_Datatype MPI_LabeledPoint) {
+  MPI_Status status;
+  int count;
+
+  //TODO: LEAKING
+  LabeledPointT *buffer = malloc(MAX_PARTITION_SIZE * sizeof(LabeledPointT));
+
+  MPI_Recv(buffer, MAX_PARTITION_SIZE, MPI_LabeledPoint, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+  MPI_Get_count(&status, MPI_LabeledPoint, &count);
+
+  LabeledPointT *result = poolAdjacentViolators(buffer, count);
+
+  MPI_Send(result, count, MPI_LabeledPoint, 0, 1, MPI_COMM_WORLD);
 }
 
 int main(argc, argv)
@@ -159,46 +180,9 @@ char *argv[];
    printf("name %s: hello world from process %d of %d\n", name, rank, size);
 
    if(rank == 0) {
-      LabeledPointT labels[21] = {{1, 1, 1} , {2, 2, 1}, {3, 3, 1}, {3, 4, 1}, {1, 5, 1}, {6, 6, 1}, {7, 7, 1}, {8, 8, 1}, {11, 9, 1}, {9, 10, 1}, {10, 11, 1}, {12, 12, 1}, {14, 13, 1}, {15, 14, 1}, {17, 15, 1}, {16, 16, 1}, {17, 17, 1}, {18, 18, 1}, {19, 19, 1}, {20, 20, 1}, {21, 21, 1}};
-
-      int destination;
-      int partition = 0;
-      
-      //distribute to workers
-      for(destination = 1; destination < size; destination++) {
-        MPI_Send(labels + partition, 7, MPI_LabeledPoint, destination, 1, MPI_COMM_WORLD);
-        partition += 7;
-      }
-      
-      MPI_Status status;
-      LabeledPointT result[21];
-      int count;
-      partition = 0;
-      for(destination = 1; destination < size; destination++) {
-         MPI_Recv(result + partition, MAX_PARTITION_SIZE, MPI_LabeledPoint, destination, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-         MPI_Get_count(&status, MPI_LabeledPoint, &count);
-         //printf("RECEIVED COUNT %d\n", count);
-         partition += count;
-      }
-
-      LabeledPointT *finalResult = pava(result, 21);
-
-      printArray(finalResult, 21);
-
+      master(MPI_LabeledPoint, size);
    } else {
-      MPI_Status status;
-      int count;
-      //LabeledPointT buffer[MAX_PARTITION_SIZE];
-
-      //TODO: LEAKING
-      LabeledPointT *buffer = malloc(MAX_PARTITION_SIZE * sizeof(LabeledPointT));
-
-      MPI_Recv(buffer, MAX_PARTITION_SIZE, MPI_LabeledPoint, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-      MPI_Get_count(&status, MPI_LabeledPoint, &count);
-
-      LabeledPointT *result = pava(buffer, count);
-
-      MPI_Send(result, count, MPI_LabeledPoint, 0, 1, MPI_COMM_WORLD);
+      partition(MPI_LabeledPoint);
    }
 
    MPI_Type_free(&MPI_LabeledPoint);
